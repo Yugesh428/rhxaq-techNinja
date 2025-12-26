ede478976b85d36c3b87be05f3207cc8cf42957f
Yugesh428 <yugeshbastola72@gmail.com>
2025-12-26 16:01:26 +0545

    route updated final commit

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express from "express";
-import dotenv, { config } from "dotenv";
+import dotenv from "dotenv";
 
 import userRoute from "./route/userRoute/userRoute";
 import taskRoute from "./route/taskRoute/taskRoute";
 import projectRoute from "./route/projectRoute/projectRoute";
-config();
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+// Routes
+app.use("/users", userRoute);
+app.use("/tasks", taskRoute);
+app.use("/projects", projectRoute);
 
 export default app;
